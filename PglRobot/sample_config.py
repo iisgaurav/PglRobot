e5804b60d47e47af5f45f344eee395a355be8e76
Gaurav Verma <gauravv2504@gmail.com>
2021-03-21 10:18:25 +0530

    Update sample_config.py

--- PglRobot/sample_config.py
+++ PglRobot/sample_config.py
@@ -18,7 +18,7 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
+    OWNER_USERNAME = "gauravv_03"
     SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001253661229
