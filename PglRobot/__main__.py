df526cebf0ca9ab131ef022053c286963b35cb99
itzgauravv <78420388+itzgauravv@users.noreply.github.com>
2021-03-28 02:42:40 +0530

    Update __main__.py

--- PglRobot/__main__.py
+++ PglRobot/__main__.py
@@ -260,7 +260,7 @@ def start(update: Update, context: CallbackContext):
     else:
         update.effective_message.reply_photo(
             PglRobotG_IMG,
-            G_START_TEXT.format(),
+            G_START_TEXT,
         )
 
 
