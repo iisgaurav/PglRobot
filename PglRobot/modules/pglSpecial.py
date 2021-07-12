f27521cfd02ffc0c871839e1fe8ce6991145a33b
Gaurav <gauravv2504@gmail.com>
2021-07-13 02:43:51 +0530

    Update pglSpecial.py

--- PglRobot/modules/pglSpecial.py
+++ PglRobot/modules/pglSpecial.py
@@ -1,4 +1,21 @@
 __help__ = """
+*Fun Commands*
+ • /runs: reply a random string from an array of replies
+ • /slap: slap a user, or get slapped if not a reply
+ • /shrug: get shrug XD
+ • /table: get flip/unflip :v
+ • /decide: Randomly answers yes/no/maybe
+ • /toss: Tosses A coin
+ • /bluetext: check urself :V
+ • /roll: Roll a dice
+ • /rlg: Join ears,nose,mouth and create an emo ;-;
+ • /shout <keyword>: write anything you want to give loud shout
+ • /weebify <text>: returns a weebified text
+ • /sanitize: always use this before /pat or any contact
+ • /pat: pats a user, or get patted
+ • /8ball: predicts using 8ball method
+
+
 *Fun Gifs and Images:*
  • `/pat`*:* pats a user by a reply to the message.
  • `/neko`*:* sends random neko image
